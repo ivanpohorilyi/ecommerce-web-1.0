9ba48e35917f7c54c25648819ecba0277ad54aab
ivanpohorilyi <thepatrickmaps@gmail.com>
2023-07-06 01:39:09 +0300

    fifth commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+  return (<div>
+    {props.orders.map(el => (
+                  <Order key={el.div} item={el} />
+                ))}
+  </div>)
+}
+
+const showNothing = () => {
+  return(<div className='empty'>
+    <h2>Товаров нет</h2>
+  </div>)
+}
+
+export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false)
 
   return (
@@ -18,7 +33,7 @@ export default function Header() {
 
             {cartOpen && (
               <div className='shop-cart'>
-                
+                {props.orders.length > 0 ? showOrders(props) : showNothing()}
               </div>
             )}
         </div>
